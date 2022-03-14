be69de87f63ee240d7778d00c2f8f467849805d3
mahadihasan2 <mahadihasan125643@gmail.com>
2022-03-14 23:39:03 +0600

    style-Update

--- js/country.js
+++ js/country.js
@@ -7,16 +7,17 @@ const displaycountry = (countries) => {
     console.log(countries)
     const allcountryHtml = countries.map(country => singlecountry(country))
     // console.log(allcountryHtml[0])
-    const containerCountry = document.getElementById('country')
+    const containerCountry = document.getElementById('countryies')
     containerCountry.innerHTML = allcountryHtml.join(' ')
 }
 
 const singlecountry = (country) => {
 
     return `
-        <div>
+        <div class="country">
             <h2>${country.name.common}</h2>
-            <img src="${country.flags.png}"
+            <h3>${country.population}</h3>
+            <img src="${country.flags.png}">
         </div>
     `
 }
